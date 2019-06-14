c78b8ce1913e126467a64d666b86f10c1a1ff4ad
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-06-14 11:04:30 +0800

    Agregando entidad Analisis

---
+++ RegistroAnalisis/Entidades/Analisis.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroAnalisis.Entidades
+{
+    public class Analisis
+    {
+        [Key]
+
+        public int AnalisisId { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime Fecha { get; set; }
+
+
+        public Analisis()
+        {
+            AnalisisId = 0;
+            UsuarioId = 0;
+            Fecha = DateTime.Now;
+        }
+    }
+}
