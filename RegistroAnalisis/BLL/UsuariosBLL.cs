5d18fe9c66b40f88773f59241b6d1b94f2f81e5e
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-06-15 12:38:25 +0800

    Agregando BLL de Usuarios

---
+++ RegistroAnalisis/BLL/UsuariosBLL.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using RegistroAnalisis.DAL;
+using RegistroAnalisis.Entidades;
+using RegistroAnalisis.BLL;
+
+namespace RegistroAnalisis.BLL
+{
+    public class UsuariosBLL
+    {
+
+        public static bool Guardar(Usuarios usuario)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                if (db.Usuarios.Add(usuario) != null)
+
+                    paso = db.SaveChanges() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Modificar(Usuarios usuario)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                paso = (db.SaveChanges() > 0);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                var eliminar = db.Usuarios.Find(id);
+                db.Entry(eliminar).State = EntityState.Deleted;
+                paso = (db.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static Usuarios Buscar(int id)
+        {
+            Usuarios usuarios = new Usuarios();
+            Contexto db = new Contexto();
+
+
+            try
+            {
+                usuarios = db.Usuarios.Find(id);
+                db.Dispose();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return usuarios;
+
+        }
+
+        public static List<Usuarios> GetList(Expression<Func<Usuarios, bool>> usuarios)
+        {
+            List<Usuarios> Lista = new List<Usuarios>();
+            Contexto db = new Contexto();
+
+            try
+            {
+                Lista = db.Usuarios.Where(usuarios).ToList();
+
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return Lista;
+
+        }
+
+    }
+}
