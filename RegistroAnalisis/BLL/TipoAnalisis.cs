2069c7892d93afdbd1a82e4bba6f1fb100a64401
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-06-15 12:36:18 +0800

    Agregando BLL TiposAnalisis

---
+++ RegistroAnalisis/BLL/TipoAnalisis.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using RegistroAnalisis.DAL;
+using RegistroAnalisis.Entidades;
+using RegistroAnalisis.BLL;
+
+namespace ProyectoFinalAplicadaI.BLL
+{
+    public class TipoAnalalisisBLL
+    {
+
+
+        public static bool Guardar(TipoAnalisis tipoanalisis)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                if (db.TipoAnalisis.Add(tipoanalisis) != null)
+
+                    paso = db.SaveChanges() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Modificar(TipoAnalisis tipoanalisis)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                db.Entry(tipoanalisis).State = EntityState.Modified;
+                paso = (db.SaveChanges() > 0);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                var eliminar = db.TipoAnalisis.Find(id);
+                db.Entry(eliminar).State = EntityState.Deleted;
+                paso = (db.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static TipoAnalisis Buscar(int id)
+        {
+            TipoAnalisis tipoanalisis = new TipoAnalisis();
+            Contexto db = new Contexto();
+
+
+            try
+            {
+                tipoanalisis = db.TipoAnalisis.Find(id);
+                db.Dispose();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return tipoanalisis;
+
+        }
+
+        public static List<TipoAnalisis> GetList(Expression<Func<TipoAnalisis, bool>> tipoanalisis)
+        {
+            List<TipoAnalisis> Lista = new List<TipoAnalisis>();
+            Contexto db = new Contexto();
+
+            try
+            {
+                Lista = db.TipoAnalisis.Where(tipoanalisis).ToList();
+
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return Lista;
+
+        }
+    }
+}
